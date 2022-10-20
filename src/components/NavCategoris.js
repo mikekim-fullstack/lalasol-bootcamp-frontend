19dcbd4c96994c5d0fa5d5ffd594c014f3a7daad
mikekim-fullstack <mikekim.fullstack@gmail.com>
2022-10-20 16:27:21 -0400

    fetched courses-category from db

--- src/components/NavCategoris.js
+++ src/components/NavCategoris.js
@@ -1,66 +1,76 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './NavCategories.css'
 import { useDispatch } from 'react-redux'
 import { setCat, setSelCatStatus } from '../slices/categorySlice'
-
+import useAxios from '../useAxios'
 const NavCategoris = () => {
+    const [sortedCat, setSortedCat] = useState(null)
+    const [categories, catError, catLoading] = useAxios({
+        method: 'GET',
+        url: '/api/course-category/',
+        headers: {
+            'Content-Type': 'Application/Json'
+        },
+
+    })
+    // const [categories, setCategories]=useState([])
     // const categories = ['HTML', 'CSS', 'JavaScript', 'Data Structure', 'ReactJS', 'Practices', 'General', 'HR']
-    const categories = [
-        {
-            "id": 1,
-            'order': 8,
-            "title": "HR",
-            "description": "HR Category"
-        },
-        {
-            "id": 2,
-            'order': 7,
-            "title": "General",
-            "description": "General Category"
-        },
-        {
-            "id": 9,
-            'order': 6,
-            "title": "Practices",
-            "description": "ReactJS Category"
-        },
-        {
-            "id": 3,
-            'order': 5,
-            "title": "ReactJS",
-            "description": "ReactJS Category"
-        },
-        {
-            "id": 4,
-            'order': 4,
-            "title": "Data Structure",
-            "description": "Data Structure Category"
-        },
-        {
-            "id": 5,
-            'order': 3,
-            "title": "JavaScript",
-            "description": "JavaScript Cagtegory"
-        },
-        {
-            "id": 6,
-            'order': 2,
-            "title": "CSS",
-            "description": "CSS Category"
-        },
-        {
-            "id": 8,
-            'order': 1,
-            "title": "HTML",
-            "description": "HTML Category"
-        }
-    ]
+    // const categories = [
+    //     {
+    //         "id": 1,
+    //         'order': 8,
+    //         "title": "HR",
+    //         "description": "HR Category"
+    //     },
+    //     {
+    //         "id": 2,
+    //         'order': 7,
+    //         "title": "General",
+    //         "description": "General Category"
+    //     },
+    //     {
+    //         "id": 9,
+    //         'order': 6,
+    //         "title": "Practices",
+    //         "description": "ReactJS Category"
+    //     },
+    //     {
+    //         "id": 3,
+    //         'order': 5,
+    //         "title": "ReactJS",
+    //         "description": "ReactJS Category"
+    //     },
+    //     {
+    //         "id": 4,
+    //         'order': 4,
+    //         "title": "Data Structure",
+    //         "description": "Data Structure Category"
+    //     },
+    //     {
+    //         "id": 5,
+    //         'order': 3,
+    //         "title": "JavaScript",
+    //         "description": "JavaScript Cagtegory"
+    //     },
+    //     {
+    //         "id": 6,
+    //         'order': 2,
+    //         "title": "CSS",
+    //         "description": "CSS Category"
+    //     },
+    //     {
+    //         "id": 8,
+    //         'order': 1,
+    //         "title": "HTML",
+    //         "description": "HTML Category"
+    //     }
+    // ]
     // ++ Output from sort ++
     //- id: category[0]
     //- title: category[1]
-    const sortedCat = Object.entries(categories)
-        .sort(([, a], [, b]) => (a.order - b.order)) // ascending by order
-        .map(([key, value_cat]) => [value_cat.id, value_cat.title])
+    // const sortedCat = Object.entries(categories)
+    //     .sort(([, a], [, b]) => (a.order - b.order)) // ascending by order
+    //     .map(([key, value_cat]) => [value_cat.id, value_cat.title])
     // console.log('sorted: ', sortedCat)
 
     const dispatch = useDispatch()
@@ -74,9 +84,19 @@ const NavCategoris = () => {
         dispatch(setSelCatStatus(true))
 
     }
+
+    useEffect(() => {
+        if (categories != null) {
+            console.log('-----categoreis----:', categories)
+            const _sortedCat = Object.entries(categories)
+                .sort(([, a], [, b]) => (a.order - b.order)) // ascending by order
+                .map(([key, value_cat]) => [value_cat.id, value_cat.title])
+            setSortedCat(_sortedCat)
+        }
+    }, [categories])
     return (
         <div className='nav__categories'>
-            {sortedCat.map((category, index) => <button key={category[0]} className='nav__btn_category' onClick={handleBtnClick} name={category[0]} dangerouslySetInnerHTML={{ __html: (category[1].replace(' ', '&nbsp;')) }}></button>)}
+            {sortedCat?.map((category, index) => <button key={category[0]} className='nav__btn_category' onClick={handleBtnClick} name={category[0]} dangerouslySetInnerHTML={{ __html: (category[1].replace(' ', '&nbsp;')) }}></button>)}
         </div>
     )
 }
---
+++ src/useAxios.js
@@ -0,0 +1,40 @@
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
+
+// process.env.REACT_API_DEBUG
+process.env.REACT_APP_DEBUG == 'true' ?
+    axios.defaults.baseURL = 'http://127.0.0.1:8000'
+    : axios.defaults.baseURL = 'https://lalasol-bootcamp-backend-production.up.railway.app'
+
+const useAxios = ({ method, url, headers = null, body = null }) => {
+    const [response, setResponse] = useState(null)
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(true)
+    console.log('process.env.REACT_APP_DEBUG: ',)
+    const fetchData = async () => {
+        try {
+            const result = await axios({
+                method,
+                url,
+                headers,
+                body,
+            })
+            // const result = await axios.get('/api/course-category/')
+            // console.log('axios: result-', result)
+            setResponse(result.data)
+        }
+        catch (error) {
+            // console.log('axois: error--', error)
+            setError(error)
+        }
+        finally {
+            setLoading(false)
+        }
+    }
+    useEffect(() => {
+        fetchData()
+    }, [])
+    return [response, error, loading]
+}
+
+export default useAxios
