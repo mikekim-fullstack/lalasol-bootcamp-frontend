1fbaa53c46a760c12c58b24626d22100af25d4cd
mikekim-fullstack <mikekim.fullstack@gmail.com>
2022-10-19 22:19:13 -0400

    syntaxhighter added

--- src/screens/CourseScreen.js
+++ src/screens/CourseScreen.js
@@ -6,7 +6,14 @@ import { getChapters, setTest, setChapters } from '../slices/chapterSlice'
 import { login } from '../slices/userSlices'
 import axios from 'axios'
 
+import { PrismAsyncLight as SyntaxHighlighter } from 'react-syntax-highlighter'
+// import js from 'react-syntax-highlighter/dist/esm/languages/hljs/javascript'
+// import html from 'react-syntax-highlighter/dist/esm/languages/hljs/javascript'
+import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs'
+import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism'
+import a11yDark from "react-syntax-highlighter/dist/esm/styles/prism/a11y-dark"
 
+// SyntaxHighlighter.registerLanguage('html', html)
 
 // import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 // pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/legacy/build/pdf.worker.min.js`
@@ -17,10 +24,11 @@ const CourseScreen = () => {
     const [chapter, setChapter] = useState(null)
     const dispatch = useDispatch()
     const [urlFile, setUrlFile] = useState(null)
+    const [htmlCode, setHtmlCode] = useState(null)
+    const [gitHubFile, setGitHubFile] = useState(null)
+    const [fileExtension, setFileExtension] = useState(null)
 
-    const handleIframeClick = (e) => {
-        console.log('--handleIframeClick--')
-    }
+
 
     const fetchFile = async (file) => {
 
@@ -34,28 +42,29 @@ const CourseScreen = () => {
             url = (file)
         }
         setUrlFile(url)
-        console.log('----urlFile: ', url)
+        console.log('----urlFile: ', url, 'file extension: ', url.split('.').pop())
 
         // -- Go and get the html file from server. --
-        /*
-        await axios.get(url,
-            {
-                headers: {
-                    "Content-type": "Application/Json",
+        if (url.split('.').pop().toLowerCase() == 'html') {
+            await axios.get(url,
+                {
+                    headers: {
+                        "Content-type": "Application/Json",
+                    }
                 }
-            }
-        )
-            .then(res => {
-                // console.log(res.data)
-                setHtml(res.data)
-            })
-        */
+            )
+                .then(res => {
+                    // console.log(res.data)
+                    setHtmlCode(res.data)
+                })
+        }
     }
     const fetchChapter = async (subjectId) => {
         await axios.get(process.env.REACT_APP_BASE_URL + `/api/chapter/${subjectId}`,
             {
                 headers: {
                     "Content-type": "Application/Json",
+
                 }
             }
         )
@@ -65,6 +74,33 @@ const CourseScreen = () => {
                 fetchFile(res.data.html)
                 console.log('------', res.data.category)
                 setChapter(res.data)
+            })
+            .catch(err => console.log('error: ', err))
+
+    }
+    const fetchGithub = async (subjectId) => {
+        const urlBase = 'https://raw.githubusercontent.com/'
+        const url1 = 'mikekim-fullstack/JS-interview/blob/master/10-hex.js'
+        // const url2 = 'https://raw.githubusercontent.com/mikekim-fullstack/HTML-CSS-reponsive-layout-single-page/main/index.html'
+        //  'https://raw.githubusercontent.com/mikekim-fullstack/seytech-css-agency/master/firebase.js'
+        const url3 = 'mikekim-fullstack/seytech-css-agency/blob/master/firebase.js'
+        const url4 = 'mikekim-fullstack/seytech-css-agency/blob/master/index.html'
+        const url = urlBase + url1.replace('/blob/', '/')
+
+        console.log('-- url Github: ', url)
+        await axios.get(url,
+            {
+                headers: {
+                    "Content-type": "Application/Json",
+                    // 'Access-Control-Allow-Origin': '*',
+                }
+            }
+        )
+            .then(res => {
+                setGitHubFile(res.data)
+                setFileExtension(url.split('.').pop().toLocaleLowerCase())
+                // console.log('------github----', res.data)
+
             })
             .catch(err => console.log('error: ', err))
 
@@ -89,23 +125,9 @@ const CourseScreen = () => {
             // console.log('--- data:', data)
         }
 
-
+        fetchGithub()
 
     }, [chapters, course_id])
-
-    // useEffect(() => {
-
-    //     // fetchAll()
-    //     if (!chapters) {
-
-    //         fetchChapter(course_id)
-
-    //         // console.log('--- data:', data)
-    //     }
-
-    //     console.log('useEffect-------2')
-    //     // fetchChapter()
-    // }, [])
 
     return (
 
@@ -116,22 +138,34 @@ const CourseScreen = () => {
             {/* <div dangerouslySetInnerHTML={{ __html: (urlLocal) }} /> */}
             {chapter && chapter.category.title == 'PDF' ?
                 urlFile && <div>
-                    <iframe className='iframe__view' onClick={handleIframeClick}
+                    <iframe className='iframe__view'
                         src={urlFile} title="description" style={{ width: '100%', height: '100vh', frameBorder: "0", border: '0' }}>
                     </iframe>
                     <br />
                     <h1 style={{ padding: '20px', fontSize: '1rem', lineHeight: '1rem' }}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus harum voluptates illum adipisci nulla sint ullam enim at natus suscipit sequi odit nemo quia numquam mollitia rem, unde possimus doloribus autem dolorum. Magni magnam ratione commodi iure voluptates aliquam, ducimus rem animi, dolorum distinctio laudantium? Natus voluptatibus autem est dolorem harum odio et animi perspiciatis mollitia provident maxime, omnis aliquid voluptatum ducimus totam. Excepturi facilis quae expedita odio quo maiores consequuntur quibusdam esse culpa impedit nostrum voluptatem natus, quidem aperiam. Voluptatum sapiente earum blanditiis rem, minus nisi mollitia aut quibusdam et amet fugiat omnis impedit, dolor ipsam animi cupiditate. Nostrum!</h1>
                 </div>
                 :
-                urlFile && <div>
-                    <iframe className='iframe__view' onClick={handleIframeClick}
-                        src={urlFile} title="description" style={{ width: '90%', height: '100vh', frameBorder: "0", border: '0' }}>
-                        <h1>CourseScreen 2</h1>
-                        <h1>CourseScreen 3</h1>
-                        <h1>CourseScreen 4</h1>
-                        <h1>CourseScreen 5</h1>
-                        <h1>CourseScreen 5</h1>
-                    </iframe>
+                <div>
+
+                    {htmlCode && <div dangerouslySetInnerHTML={{ __html: htmlCode }} />}
+                    {/* <iframe className='iframe__view' src={urlFile} title="description">
+                    </iframe> */}
+                    <h2>Code Snippet</h2>
+                    <div style={{ textAlign: 'center', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                        {fileExtension && <SyntaxHighlighter language={fileExtension} style={a11yDark} showLineNumbers="true"
+                            customStyle={{ border: 'none', borderRadius: '0', boxShadow: 'none', width: '90%', padding: '20px 0px', height: '400px' }}
+                            children={gitHubFile && gitHubFile}
+                        />}
+                        <iframe style={{ margin: '0', width: '90%', frameBorder: '0' }} src="https://www.youtube.com/embed/y881t8ilMyc" allowFullscreen></iframe>
+
+
+                    </div>
+
+                    <h1>CourseScreen 2</h1>
+                    <h1>CourseScreen 3</h1>
+                    <h1>CourseScreen 4</h1>
+                    <h1>CourseScreen 5</h1>
+                    <h1>CourseScreen 5</h1>
 
                 </div>
             }
