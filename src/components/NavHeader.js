71e49a574b69b4675d9b443a13ec9277291ad077
mikekim-fullstack <mikekim.fullstack@gmail.com>
2022-10-24 21:13:40 -0400

    fix bug on mobile mode

--- src/components/NavHeader.js
+++ src/components/NavHeader.js
@@ -96,7 +96,8 @@ const NavHeader = () => {
                 <div className='nav__user'>
                     {/* <div className='logout' onClick={handleLogout}>Logout</div> */}
                     <i className="fa-regular fa-bell"></i>
-                    <button onClick={handleLogout}>{user && `${user?.first_name[0]?.toUpperCase()}${user?.last_name[0]?.toUpperCase()}`}</button>
+                    {/* <button className='btn_user_profile' onClick={handleLogout}>VS</button> */}
+                    <button className='btn_user_profile' onClick={handleLogout}>{user && `${user?.first_name[0]?.toUpperCase()}${user?.last_name[0]?.toUpperCase()}`}</button>
                 </div>
                 {selectedCat && <NavSubmenu className={showSubmenu && 'nav-open'} />}
             </div>
