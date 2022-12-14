0bc702a0af374183db455aaa2a6eac388db0a093
mikekim-fullstack <mikekim.fullstack@gmail.com>
2022-12-13 23:02:23 -0500

    code shell execution with database added

--- src/screens/PracticeScreen.js
+++ src/screens/PracticeScreen.js
@@ -7,27 +7,136 @@ import CodeMirror from '@uiw/react-codemirror';
 import { javascript } from '@codemirror/lang-javascript';
 import { tags as t } from '@lezer/highlight';
 import { tokyoNight, tokyoNightInit } from '@uiw/codemirror-theme-tokyo-night';
+import { historyField } from '@codemirror/commands';
+import { getUser } from '../slices/userSlices';
+import { useSelector } from 'react-redux';
 
 const PracticeScreen = () => {
 
     const { practice_id } = useParams()
+    const user = useSelector(getUser)
+
+    const serializedState = localStorage.getItem('myEditorState');
+    const value = localStorage.getItem('myValue') || '// Let\'s start LaLaSol coding!';
+    // When custom fields should be serialized, you can pass them in as an object mapping property names to fields.
+    // See [toJSON](https://codemirror.net/docs/ref/#state.EditorState.toJSON) documentation for more details
+    const stateFields = { history: historyField };
 
     const [code, setCode] = useState(null)
     const [codeResult, setCodeResult] = useState(null)
     const resultRef = useRef(null)
+    const [fetchedCode, setFetchedCode] = useState(null)
+    const [selCode, setSelCode] = useState(null)
+
+    const fetchJSCode = async () => {
+        const url = axios.defaults.baseURL + '/api/student-js-view/' + user.id
+        await axios({
+            method: 'GET',
+            url,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+
+        })
+            .then(res => {
+                console.log('fetchJSCode:' + url, res.data)
+                setFetchedCode(res.data)
+            })
+            .catch(err => console.log('fetchJSCode-error:' + url, err))
+    }
+    const onClickFetchedCode = (e, codeId) => {
+        const selCodeItem = fetchedCode.filter(item => item.id == codeId)[0]
+
+        console.log('selCodeItem-', selCodeItem)
+        localStorage.setItem('myValue', selCodeItem.js_code);
+        setSelCode(selCodeItem)
+
+        setCode(selCodeItem.js_code)
+
+    }
+    const onSubmit = (e, id) => {
+        e.preventDefault()
+        const js_code = localStorage.getItem('myValue')
+
+
+        if (id >= 0) {
+
+            const url = axios.defaults.baseURL + '/api/student-js-update/' + selCode.id
+            const bodyData = { 'title': selCode.title, 'js_code': js_code }
+            console.log('onSubmit: ', selCode, ', value: ', js_code, ', bodyData', JSON.stringify(bodyData))
+            axios({
+                method: 'PATCH',
+                url,
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                data: JSON.stringify(bodyData)
+
+
+            })
+                .then(res => {
+                    console.log('onSubmit-fetchJSCode:' + url, res.data)
+                    fetchJSCode()
+                    setSelCode(res.data)
+                })
+                .catch(err => console.log('fetchJSCode-error:' + url, err))
+        }
+        else {
+            const url = axios.defaults.baseURL + '/api/student-js-create/'
+            const bodyData = { 'title': selCode.title, 'js_code': js_code, 'student': user?.id }
+            console.log('onSubmit: ', selCode, ', value: ', js_code, ', bodyData', JSON.stringify(bodyData))
+            axios({
+                method: 'POST',
+                url,
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                data: JSON.stringify(bodyData)
+
+
+            })
+                .then(res => {
+                    console.log('onSubmit-fetchJSCode:' + url, res.data)
+                    fetchJSCode()
+                    setSelCode(res.data)
+                })
+                .catch(err => console.log('fetchJSCode-error:' + url, err))
+        }
+    }
+
+    /*
+console.log('hello from postman3')
+console.log('Hello World \n--------------\n')
+
+const display = (data)=>{
+console.log('data=',data)
+}
+
+const data=[10,7,8,1,2,3,4]
+for (c of data){
+display(c)
+}
+const sortedData = data.sort((a,b)=>a>b?1:a<b?-1:0)
+console.log('sortedData=', sortedData)
+    */
+
+    const onClickCreateCode = (e) => {
+        setSelCode({ title: 'New Code', id: -1, js_code: "// Let\'s start LaLaSol coding!" })
+    }
     const onClickRunCode = (e) => {
         const url = 'https://express-shell-execute-js-production.up.railway.app/api/'
-        if (!code) return
-        const data = { 'user': 's1@gmail.com', 'id': '2', 'js-code': code }
-        console.log('data: ', JSON.stringify(data))
+        if (!selCode?.js_code) return
+        const data = { 'user': user.email, 'id': user.id, 'js-code': selCode.js_code }
+        // console.log('data: ', JSON.stringify(data))
         axios({
             method: 'POST',
             url,
-            headers: { 'Content-Type': 'Application/Json' },
+            headers: { 'Content-Type': 'application/json' },
+
             data: JSON.stringify(data)
         })
             .then(res => {
-                console.log('onClickRunCode-' + url, res.data, resultRef)
+                // console.log('onClickRunCode-' + url, res.data, resultRef)
                 setCodeResult(res.data)
 
                 resultRef.current.innerHTML = res.data.replace(/\n/g, "<br />")
@@ -37,32 +146,62 @@ const PracticeScreen = () => {
     useEffect(() => {
 
     }, [codeResult])
+    useEffect(() => {
+        fetchJSCode()
+    }, [])
     return (
+
         <div className='practice__screen'>
-            {/* PracticeScreen {practice_id} */}
+            { }
+            <div className='code-lists'>
+                <div className='create-code'>
+                    <button onClick={onClickCreateCode}>Create New</button>
+                </div>
+                {fetchedCode?.map(item => {
+                    return <div key={item.id} className='item'>
+                        <span>{item.title}</span>
+                        <button onClick={e => onClickFetchedCode(e, item.id)}>Show Code</button>
+
+                    </div>
+                })}
+            </div>
             <CodeMirror
-                value="// Let's start LaLaSol coding!"
+                value={selCode?.js_code}
+                // initialState={
+                //     serializedState
+                //         ? {
+                //             json: JSON.parse(serializedState || ''),
+                //             fields: stateFields,
+                //         }
+                //         : undefined
+                // }
                 minWidth={"100%"}
-                // style={{ marginLeft: "auto", marginRight: "auto" }}
                 height="100vh"
                 theme={tokyoNight}
+                // highlightActiveLine={false}
+                // rectangularSelection={false}
+                // lineNumbers='false'
                 extensions={[javascript({ jsx: false })]}
                 onChange={(value, viewUpdate) => {
                     setCode(value)
-                    // console.log('value:', value);
+                    setSelCode({ ...selCode, js_code: value })
+                    localStorage.setItem('myValue', value);
+
+                    const state = viewUpdate.state.toJSON(stateFields);
+                    localStorage.setItem('myEditorState', JSON.stringify(state));
                 }}
             />
             <div className='cm-result'>
+                {
+                    selCode?.title &&
+                    <form onSubmit={e => onSubmit(e, selCode?.id)}>
+                        <input type='text' name='title' value={selCode.title} onChange={e => setSelCode({ ...selCode, 'title': e.target.value })} />
+                        <button type='submit'>Save</button>
+                    </form>
+                }
                 <button onClick={e => onClickRunCode(e)}>Run Code</button>
                 {<div className='code-result' ref={resultRef}></div>}
-                {/* {codeResult && <CodeMirror
-                    value={JSON.stringify(codeResult)}
-                    minWidth={"100%"}
-                    // style={{ marginLeft: "auto", marginRight: "auto" }}
-                    height="100vh"
-                    theme={tokyoNight}
 
-                />} */}
             </div>
 
 
